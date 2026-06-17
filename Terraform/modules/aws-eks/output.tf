e6340090f91901ffb513fe473377c622379cffd7
vkharishb <v.harish.bodapati@gmail.com>
2026-06-17 11:20:35 +0530

    synching

--- Terraform/modules/aws-eks/output.tf
+++ Terraform/modules/aws-eks/output.tf
@@ -1,41 +1,30 @@
-# Output the EKS cluster endpoint
-output "cluster_endpoint" {
-  description = "Endpoint for your Kubernetes API server"
-  value       = module.eks_cluster.cluster_endpoint
-}
-
-# Output the EKS cluster name
 output "cluster_name" {
-  description = "The name of the EKS cluster"
+  description = "EKS cluster name"
   value       = module.eks_cluster.cluster_name
 }
 
-# Output the EKS cluster version
-output "cluster_version" {
-  description = "The Kubernetes version for the cluster"
-  value       = module.eks_cluster.cluster_version
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks_cluster.cluster_endpoint
 }
 
-# Output the cluster security group ID
-output "cluster_security_group_id" {
-  description = "Security group ID attached to the EKS cluster"
-  value       = module.eks_cluster.cluster_security_group_id
-}
-
-# Output the managed node group IDs
-output "eks_managed_node_groups" {
-  description = "Map of attribute maps for all EKS managed node groups created"
-  value       = module.eks_cluster.eks_managed_node_groups
-}
-
-# Output cluster ARN
 output "cluster_arn" {
-  description = "The Amazon Resource Name (ARN) of the cluster"
+  description = "EKS cluster ARN"
   value       = module.eks_cluster.cluster_arn
 }
 
-# Output OIDC provider ARN
-output "oidc_provider_arn" {
-  description = "ARN of the OIDC Provider for EKS"
-  value       = try(module.eks_cluster.oidc_provider_arn, null)
+output "cluster_certificate_authority_data" {
+  description = "EKS cluster CA data"
+  value       = module.eks_cluster.cluster_certificate_authority_data
+  sensitive   = true
+}
+
+output "node_security_group_id" {
+  description = "EKS node security group ID"
+  value       = module.eks_cluster.node_security_group_id
+}
+
+output "cluster_security_group_id" {
+  description = "EKS cluster security group ID"
+  value       = module.eks_cluster.cluster_security_group_id
 }
