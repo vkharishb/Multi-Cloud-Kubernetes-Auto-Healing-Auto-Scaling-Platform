baad0488f04a14b478c7bac4edf9c1cde622796c
vkharishb <v.harish.bodapati@gmail.com>
2026-06-17 11:27:22 +0530

    Terrfaorm env variable

--- Terraform/env/dev/aws/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "mcp-dev-tfstate"
-    key          = "dev/aws/terraform.tfstate"
-    region       = "ap-south-1"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
--- Terraform/env/dev/azure/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "mcp-dev-tfstate-rg"
-    storage_account_name  = "mcpdevtfstate"
-    container_name        = "tfstate"
-    key                   = "dev/azure/terraform.tfstate"
-  }
-}
